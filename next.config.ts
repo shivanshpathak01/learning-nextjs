5525ce76aae3c7a1896d9d35662253766cce4521
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-01-28 16:14:16 +0530

    next config modified

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images:{
     domains:['images.unsplash.com','res.cloudinary.com']
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
