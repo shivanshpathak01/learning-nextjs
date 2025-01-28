6e16a58c7676b72d4f201d6de866b5181a6c6927
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-01-28 16:13:46 +0530

    eslint modified

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'prefer-const': 'off',
+      'no-var': 'off',
+    },
+  }
 ];
 
 export default eslintConfig;
